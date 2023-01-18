2cb0037df8f835e7670a0231851a311e0e49d72a
goverdhan07 <66107302+goverdhan07@users.noreply.github.com>
2023-01-19 02:03:31 +0530

    feat: created getAllEmployees REST API

--- src/main/java/net/projects/springboot/SpringbootBackendApplication.java
+++ src/main/java/net/projects/springboot/SpringbootBackendApplication.java
@@ -1,13 +1,28 @@
 package net.projects.springboot;
 
+import net.projects.springboot.model.Employee;
+import net.projects.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
 
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Employee employee = new Employee();
+		employee.setFirstName("Varsha");
+		employee.setLastName("Biradar");
+		employee.setEmailId("varshabiradar@gmail.com");
+		employeeRepository.save(employee);
+	}
 }
--- src/main/java/net/projects/springboot/controller/EmployeeController.java
+++ src/main/java/net/projects/springboot/controller/EmployeeController.java
@@ -1,4 +1,23 @@
 package net.projects.springboot.controller;
 
+import net.projects.springboot.model.Employee;
+import net.projects.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/employees")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
 }
